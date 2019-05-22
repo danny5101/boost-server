4e321f316c86e3bd4d105c4de1d609c4649d45f3
Qingpeng Ma <danny510-1@163.com>
2019-05-22 17:16:48 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # boost-server
 boost a tiny static web server by NodeJS anywhere anytime
+
+## 安装
+
+```
+npm install -g boost-server
+```
+
+## 使用方法
+
+```
+boost-server # 把当前文件夹作为静态资源服务器根目录
+
+boost-server -p 8080 # 设置端口号为8080
+
+boost-server -h localhost # 设置host为localhost
+
+boost-server -d /usr # 设置根目录为 /usr
+```
--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const mime = require('./helper/mime');
 const compress = require('./helper/compress');
 const range = require('./helper/range');
 const isFresh = require('./helper/cache');
+const openUrl = require('./helper/openUrl');
 
 const stat = util.promisify(fs.stat);
 const readdir = util.promisify(fs.readdir);
@@ -18,58 +19,69 @@ const tplPath = path.join(__dirname, './template/dir.tpl');
 const source = fs.readFileSync(tplPath);
 const template = handlebars.compile(source.toString());
 
-const server = http.createServer((req, res) => {
-    const filePath = path.join(conf.root, req.url);
-    (async () => {
-        try {
-            const stats = await stat(filePath);
-            if (stats.isFile()) {
-                const contentType = mime(filePath);
-                res.setHeader('Content-Type', contentType);
+class Server {
+    constructor(config) {
+        this.conf = Object.assign({}, conf, config);
+    }
 
-                if (isFresh(stats, req, res)) {
-                    res.statusCode = 304;
-                    res.end();
-                    return;
+    start() {
+        const server = http.createServer((req, res) => {
+            const filePath = path.join(this.conf.root, req.url);
+            (async () => {
+                try {
+                    const stats = await stat(filePath);
+                    if (stats.isFile()) {
+                        const contentType = mime(filePath);
+                        res.setHeader('Content-Type', contentType);
+        
+                        if (isFresh(stats, req, res)) {
+                            res.statusCode = 304;
+                            res.end();
+                            return;
+                        }
+        
+                        let rs;
+                        const {code, start, end} = range(stats.size, req, res);
+                        if (code === 200) {
+                            res.statusCode = 200;
+                            rs = fs.createReadStream(filePath);
+                        } else if (code === 206) {
+                            res.statusCode = 206;
+                            rs = fs.createReadStream(filePath,{start,end});
+                        }
+                        if (filePath.match(this.conf.compress)) {
+                            rs = compress(rs, req, res);
+                        }
+                        rs.pipe(res);
+                    } else if (stats.isDirectory()) {
+                        const files = await readdir(filePath);
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'text/html');
+                        const dir = path.relative(this.conf.root, filePath)
+                        const data = {
+                            title: path.basename(filePath),
+                            dir: dir ? `/${dir}` : '',
+                            files
+                        };
+                        res.end(template(data));
+                    }
+                } catch (error) {
+                    if(error) {
+                        console.error(error);
+                        res.statusCode = 404;
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.end(`${filePath} is not a directory or file`);
+                    }
                 }
+            })();
+        });
 
-                let rs;
-                const {code, start, end} = range(stats.size, req, res);
-                if (code === 200) {
-                    res.statusCode = 200;
-                    rs = fs.createReadStream(filePath);
-                } else if (code === 206) {
-                    res.statusCode = 206;
-                    rs = fs.createReadStream(filePath,{start,end});
-                }
-                if (filePath.match(conf.compress)) {
-                    rs = compress(rs, req, res);
-                }
-                rs.pipe(res);
-            } else if (stats.isDirectory()) {
-                const files = await readdir(filePath);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                const dir = path.relative(conf.root, filePath)
-                const data = {
-                    title: path.basename(filePath),
-                    dir: dir ? `/${dir}` : '',
-                    files
-                };
-                res.end(template(data));
-            }
-        } catch (error) {
-            if(error) {
-                console.error(error);
-                res.statusCode = 404;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(`${filePath} is not a directory or file`);
-            }
-        }
-    })();
-});
+        server.listen(this.conf.port, this.conf.hostname, () => {
+            const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+            console.info(`Server started at ${chalk.green(addr)}`);
+            openUrl(addr);
+        });
+    }
+}
 
-server.listen(conf.port, conf.hostname, () => {
-    const addr = `http://${conf.hostname}:${conf.port}`
-    console.info(`Server started at ${chalk.green(addr)}`)
-});
+module.exports = Server;
